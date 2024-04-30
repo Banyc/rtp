80fb96db522a10abc67e5485ed78eef53e540ee3
Banyc <36535895+Banyc@users.noreply.github.com>
2024-04-30 13:25:38 +0800

    feat(wireshark): detect out of order data packets

--- wireshark/rtp.dissector.lua
+++ wireshark/rtp.dissector.lua
@@ -1,6 +1,6 @@
-local rtp_name = "my_rtp"
+local NAME = "my_rtp"
 
-local rtp = Proto(rtp_name, rtp_name)
+local rtp = Proto(NAME, NAME)
 
 local MAX_NUM_ACK = 128
 
@@ -10,26 +10,44 @@ local KILL_CMD = 2
 
 local pf_ack = {}
 for i = 1, MAX_NUM_ACK do
-    pf_ack[i] = ProtoField.uint64(rtp_name .. ".ack." .. i, "ack." .. i)
+    pf_ack[i] = ProtoField.uint64(NAME .. ".ack." .. i, "ack." .. i)
     rtp.fields[#rtp.fields + 1] = pf_ack[i]
 end
-pf_ack.this = ProtoField.uint64(rtp_name .. ".ack", "ack")
+pf_ack.this = ProtoField.uint64(NAME .. ".ack", "ack")
 rtp.fields[#rtp.fields + 1] = pf_ack.this
 
 local pf_data = {
-    this    = ProtoField.uint64(rtp_name .. ".data", "data"),
-    seq     = ProtoField.uint64(rtp_name .. ".data.seq", "seq"),
-    payload = ProtoField.bytes(rtp_name .. ".data.payload", "payload"),
+    this    = ProtoField.uint64(NAME .. ".data", "data"),
+    seq     = ProtoField.uint64(NAME .. ".data.seq", "seq"),
+    payload = ProtoField.bytes(NAME .. ".data.payload", "payload"),
 }
 rtp.fields[#rtp.fields + 1] = pf_data.this
 rtp.fields[#rtp.fields + 1] = pf_data.seq
 rtp.fields[#rtp.fields + 1] = pf_data.payload
 
-local pf_kill = ProtoField.bool(rtp_name .. ".kill", "kill")
+local pf_kill = ProtoField.bool(NAME .. ".kill", "kill")
 rtp.fields[#rtp.fields + 1] = pf_kill
 
+local ef_out_of_order = ProtoExpert.new(NAME .. ".data.seq.out_of_order.expert", "Seq out of order",
+                                        expert.group.COMMENTS_GROUP, expert.severity.NOTE)
+out_of_order = {
+    last_seq = {},
+    packets = {},
+}
+
+rtp.experts = { ef_out_of_order }
+
+function five_tuple_string(pktinfo)
+    local key =
+        tostring(pktinfo.net_src) .. ":" ..
+        tostring(pktinfo.src_port) .. ":" ..
+        tostring(pktinfo.net_dst) .. ":" ..
+        tostring(pktinfo.dst_port)
+    return key
+end
+
 function rtp.dissector(tvbuf, pktinfo, root)
-    pktinfo.cols.protocol:set(rtp_name)
+    pktinfo.cols.protocol:set(NAME)
 
     local pktlen = tvbuf:reported_length_remaining()
 
@@ -38,6 +56,7 @@ function rtp.dissector(tvbuf, pktinfo, root)
     local pos = 0
     local ack_index = 1
     local info = ""
+    local key = five_tuple_string(pktinfo)
 
     for i = 1, MAX_NUM_ACK do
         if pktlen <= pos then
@@ -47,7 +66,7 @@ function rtp.dissector(tvbuf, pktinfo, root)
         local cmd = tvbuf:range(pos, 1)
         pos = pos + 1
         if cmd:uint() == ACK_CMD then
-            -- tree:add(pf_ack.this, cmd)
+            tree:add(pf_ack.this, cmd)
             
             local seq = tvbuf:range(pos, 8)
             pos = pos + 8
@@ -56,7 +75,7 @@ function rtp.dissector(tvbuf, pktinfo, root)
 
             info = info .. " Ack=" .. seq:uint64()
         elseif cmd:uint() == DATA_CMD then
-            -- tree:add(pf_data.this, cmd)
+            tree:add(pf_data.this, cmd)
 
             local seq = tvbuf:range(pos, 8)
             pos = pos + 8
@@ -70,6 +89,21 @@ function rtp.dissector(tvbuf, pktinfo, root)
             tree:add(pf_data.payload, payload)
 
             info = info .. " Seq=" .. seq:uint64()
+
+            -- out of order
+            if not pktinfo.visited then
+                if not (out_of_order.last_seq[key] == nil)
+                   and seq:uint64() <= out_of_order.last_seq[key] then
+                    out_of_order.packets[pktinfo.number] = true
+                else
+                    out_of_order.last_seq[key] = seq:uint64()
+                end
+            end
+            if out_of_order.packets[pktinfo.number] then
+                -- print("out of order", key, seq:uint64(), out_of_order.last_seq[key])
+                tree:add_tvb_expert_info(ef_out_of_order, seq)
+                info = info .. " [out of order]"
+            end
         elseif cmd:uint() == KILL_CMD then
             tree:add(pf_kill, cmd)
             
