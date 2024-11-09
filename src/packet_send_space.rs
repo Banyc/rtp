7abb59882529a9b5dcfb7d396fa9396d7a64f1b1
Banyc <36535895+Banyc@users.noreply.github.com>
2024-11-10 04:19:52 +0800

    fix: send window leaks

--- src/packet_send_space.rs
+++ src/packet_send_space.rs
@@ -111,7 +111,13 @@ impl PacketSendSpace {
             let Some(p) = p.take() else {
                 continue;
             };
-            if s == *self.send_wnd.start().unwrap() {
+            loop {
+                let Some((_, send_wnd_head)) = self.send_wnd.iter().nth(0) else {
+                    break;
+                };
+                if send_wnd_head.is_some() {
+                    break;
+                }
                 self.send_wnd.pop();
             }
 
